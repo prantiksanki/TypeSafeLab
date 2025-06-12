b76057f3912cf671abf10273147a149ef27eafd9
Prantik Sanki <prantiksanki@gmail.com>
2025-06-13 01:24:11 +0530

    Lec - 3

---
+++ Assignment-1.ts
@@ -0,0 +1,43 @@
+// NOOB Practice
+
+// // TODO - 1
+
+// var  a = 10 ; 
+// var  b = 20 ;
+
+// console.log(a+b) ; 
+
+// // TODO - 2
+
+// var c = 0 ; 
+
+// try{
+//     const d =a/c ;
+//     console.log(d) ;
+// }
+// catch(e)
+// {
+//     console.log("Result is: "+e) ;
+// }
+
+
+// TODO -3
+
+
+
+
+
+// PRO
+
+// @ts-ignore   -------> Used to catch error 
+function sum (a:number , b:number):number
+{
+    return a+b ;
+}
+
+//  console.log(sum(10,"Prantik")) ;   //WE CANT DO IT LIKE THIS
+ console.log(sum(10,20)) ;  // THIS IS OKAY
+
+
+
+
---
+++ first.ts
@@ -0,0 +1,6 @@
+let num = 5 ;
+
+// num = "vinod";  // It is incorrect
+
+
+console.log(num); // Output: 55 
